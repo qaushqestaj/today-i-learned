799d71f406416c70c8e07020265a855f2a66ec67
qaushqestaj <qaushqestaj@hotmail.com>
2024-06-20 13:05:24 +0200

    Working with form in React way

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './style.css';
 
 const CATEGORIES = [
@@ -46,42 +47,140 @@ const initialFacts = [
 ];
 
 function App() {
+  const [showForm, setShowFrom] = useState(false);
+  const [facts, setFacts] = useState(initialFacts);
+
   return (
     <>
-      <header className="header">
-        <div className="logo">
-          <img
-            src="logo.png"
-            height="68"
-            width="68"
-            alt="Today I Learned Logo"
-          />
-          <h1>Today I Learned</h1>
-        </div>
-        <button className="btn btn-large btn-open">Share a fact</button>
-      </header>
-
-      <NewFactForm />
+      <Header showForm={showForm} setShowFrom={setShowFrom} />
+      {showForm ? (
+        <NewFactForm setFacts={setFacts} setShowForm={setShowFrom} />
+      ) : null}
 
       <main className="main">
         <CategoryFilter />
-        <FactList />
+        <FactList facts={facts} />
       </main>
     </>
   );
 }
 
-function NewFactForm() {
-  return <form className="fact-form">Form</form>;
+function Header({ showForm, setShowFrom }) {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button
+        className="btn btn-large btn-open"
+        onClick={() => setShowFrom((show) => !show)}
+      >
+        {showForm ? 'Close' : 'Share a fact'}
+      </button>
+    </header>
+  );
+}
+
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === 'http:' || url.protocol === 'https:';
+}
+
+function NewFactForm({ setFacts, setShowForm }) {
+  const [text, setText] = useState('');
+  const [source, setSource] = useState('');
+  const [category, setCategory] = useState('');
+  const textLength = text.length;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // Check if data is valid. If so, create a new fact
+
+    if (text && isValidHttpUrl(source) && category && textLength <= 200) {
+      // Create a new fact object
+      const newFact = {
+        id: Math.round(Math.random() * 100000000),
+        text,
+        source,
+        category,
+        votesInteresting: 11,
+        votesMindblowing: 2,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear(),
+      };
+
+      // add the fact to the UI: add the fact to state
+      setFacts((facts) => [newFact, ...facts]);
+
+      // reset input fields
+      setText('');
+      setSource('');
+      setCategory('');
+
+      // Close the form
+      setShowForm(false);
+    }
+  }
+
+  return (
+    <form className="fact-form" onSubmit={handleSubmit}>
+      {' '}
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span>{200 - textLength}</span>
+      <input
+        value={source}
+        type="text"
+        placeholder="Trustworthy source..."
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Choose category:</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button class="btn btn-large">Post</button>
+    </form>
+  );
 }
 
 function CategoryFilter() {
-  return <aside>filter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {CATEGORIES.map((cat) => (
+          <li key={cat.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
-function FactList() {
-  const facts = initialFacts;
-
+function FactList({ facts }) {
   return (
     <section>
       <ul className="facts-list">
@@ -98,7 +197,7 @@ function Fact({ fact }) {
     <li className="fact">
       <p>
         {fact.text}
-        <a className="source" href={fact.source} target="_blank">
+        <a className="source" href={fact.source} target="blank">
           (Source)
         </a>
       </p>
