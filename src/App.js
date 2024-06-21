a8daccbe7bd453f1d56d849babe39d9d8ad8a184
qaushqestaj <qaushqestaj@hotmail.com>
2024-06-21 13:48:36 +0200

    Finishing touches with facts and scrolling

--- src/App.js
+++ src/App.js
@@ -13,40 +13,6 @@ const CATEGORIES = [
   { name: 'news', color: '#8b5cf6' },
 ];
 
-const initialFacts = [
-  {
-    id: 1,
-    text: 'React is being developed by Meta (formerly facebook)',
-    source: 'https://opensource.fb.com/',
-    category: 'technology',
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: 'Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%',
-    source:
-      'https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids',
-    category: 'society',
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: 'Lisbon is the capital of Portugal',
-    source: 'https://en.wikipedia.org/wiki/Lisbon',
-    category: 'society',
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
-
 function App() {
   const [showForm, setShowFrom] = useState(false);
   const [facts, setFacts] = useState([]);
@@ -139,18 +105,6 @@ function NewFactForm({ setFacts, setShowForm }) {
     // Check if data is valid. If so, create a new fact
 
     if (text && isValidHttpUrl(source) && category && textLength <= 200) {
-      // Create a new fact object
-      // const newFact = {
-      //   id: Math.round(Math.random() * 100000000),
-      //   text,
-      //   source,
-      //   category,
-      //   votesInteresting: 11,
-      //   votesMindblowing: 2,
-      //   votesFalse: 0,
-      //   createdIn: new Date().getFullYear(),
-      // };
-
       // Upload fact to Supabase and recieve the new fact object
       setIsUploading(true);
       const { data: newFact, error } = await supabase
@@ -258,6 +212,8 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     setIsUpdating(true);
@@ -277,6 +233,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed ? <span className="disputed">[⛔DISPUTED]</span> : null}
         {fact.text}
         <a className="source" href={fact.source} target="blank">
           (Source)
